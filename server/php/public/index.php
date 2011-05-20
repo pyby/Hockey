864522cfe721d8ad183799cb80b58a019b0630d7
Pyby <py.bertholon@mac.com>
2011-05-20 16:29:17 +0200

    Add a form on main iOS Store page to enter the store name. Just redirect to the store if it exists.

--- server/php/public/index.php
+++ server/php/public/index.php
@@ -13,6 +13,20 @@ if(!empty($_GET))
 	}
 }
 
+if(!empty($_POST['store']))
+{
+	$key = $_POST['store'];
+	
+	$folder = dirname(__FILE__) . "/" . $key;
+    if(file_exists($folder))
+    {
+    	header('Location: '.$key); 
+    	die();
+    }
+    
+    $noStore = true;
+}
+
 ?>
 
 <html>
@@ -25,6 +39,17 @@ if(!empty($_GET))
 			<h1>iOS Store</h1>
 			<h3>your private AppStore</h3>
 			<h5><a href="https://github.com/pyby/Hockey">An open source project on GitHub</a></h5>
+			<h4>My store :
+			<form method=post action="index.php">
+			<input type="text" name="store" size=40 />
+			<br />
+			<input type="submit" value="Enter">
+			</from>
+			</h4>
+<?php
+if ($noStore)
+	echo "<h5>Store doesn't exist</h5>";
+?>
 		</center>
 	</body>
 </html>
